2487d1175123c6bbb6dc4834774d8f05fd807dad
Joshua Caron <jcaron@ccs.neu.edu>
2013-08-04 21:14:48 -0400

    Updated layouts based on queries of width

--- htdocs/index.php
+++ htdocs/index.php
@@ -1,9 +1,18 @@
 <html>
 <head>
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
 	<link rel="stylesheet" type="text/css" href="css/normalize.css">
-	<link rel="stylesheet" type="text/css" href="css/style.css">
-	<link rel="stylesheet" type="text/css" href="css/layout.css">
 	<link rel='stylesheet' type='text/css' href='http://fonts.googleapis.com/css?family=Open+Sans:400,700'>
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+
+	<link 
+		rel="stylesheet" type="text/css" href="css/desktop.layout.css"
+		media="only screen and (min-width:769px)">
+	<link rel="stylesheet" type="text/css" href="css/tablet.layout.css"
+		media="only screen and (min-width:481px) and (max-width:768px)">
+	<link rel="stylesheet" type="text/css" href="css/mobile.layout.css"
+		media="only screen and (max-width:480px)">
+
 </head>
 
 <body>
@@ -22,6 +31,20 @@
 					<div class="featured image">
 						<img alt="project image" />
 					</div>
+					<p>This is some filler text. This would dexcribe a
+					project that I'm doing. It seems really awesome and fun.
+					</p>
+					<p>Here is another paragraph. Yay fun stuff.</p>
+					<p>Be careful that there isn't too much text before it would 
+					overflow off the end...
+					</p>
+					<p>This is some filler text. This would dexcribe a
+					project that I'm doing. It seems really awesome and fun.
+					</p>
+					<p>Here is another paragraph. Yay fun stuff.</p>
+					<p>Be careful that there isn't too much text before it would 
+					overflow off the end...
+					</p>
 				</div>
 			</div>
 			<div class="project">
